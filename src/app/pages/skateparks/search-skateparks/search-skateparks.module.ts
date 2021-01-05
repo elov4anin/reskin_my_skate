1faf7b0ffcc4552fc1a9506c8afde56d64588cbb
Sergey Chugaev <elov4anin@gmail.com>
2021-01-05 21:33:35 +0500

    modal-filter-skateparks.component.html map view

--- src/app/pages/skateparks/search-skateparks/search-skateparks.module.ts
+++ src/app/pages/skateparks/search-skateparks/search-skateparks.module.ts
@@ -1,20 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { SearchSkateparksPageRoutingModule } from './search-skateparks-routing.module';
+import {SearchSkateparksPageRoutingModule} from './search-skateparks-routing.module';
 
-import { SearchSkateparksPage } from './search-skateparks.page';
+import {SearchSkateparksPage} from './search-skateparks.page';
+import {GoogleMapComponent} from "./google-map/google-map.component";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    SearchSkateparksPageRoutingModule
-  ],
-  declarations: [SearchSkateparksPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        SearchSkateparksPageRoutingModule
+    ],
+    declarations: [
+        SearchSkateparksPage,
+        GoogleMapComponent,
+    ]
 })
 export class SearchSkateparksPageModule {}
