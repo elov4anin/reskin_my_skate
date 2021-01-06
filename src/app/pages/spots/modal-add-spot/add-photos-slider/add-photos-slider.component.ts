ae3596e41fc4ce106fc49c7ea87d5f9bf4ec656e
Sergey Chugaev <elov4anin@gmail.com>
2021-01-06 20:33:39 +0500

    modal-add-spot.component.html add slider photo

--- src/app/pages/spots/modal-add-spot/add-photos-slider/add-photos-slider.component.ts
+++ src/app/pages/spots/modal-add-spot/add-photos-slider/add-photos-slider.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import { sliders } from 'src/app/pages/skateparks/demodata';
+import {ISlideInfo} from "../../../skateparks/skateparks.interfaces";
 
 @Component({
   selector: 'app-add-photos-slider',
@@ -6,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-photos-slider.component.scss'],
 })
 export class AddPhotosSliderComponent implements OnInit {
+  @Input() sliders: ISlideInfo[] = sliders;
+  @ViewChild('upload') uploadRef: ElementRef;
 
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 16,
+    width: 60,
+  };
   constructor() { }
 
   ngOnInit() {}
 
+  uploadImage($event: Event) {
+
+  }
+
+  triggerUploadImage() {
+    this.uploadRef.nativeElement.click();
+  }
 }
