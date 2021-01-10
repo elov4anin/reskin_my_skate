9d2cb3a65d9c33e55de0dd47eca3049559cc8da2
Sergey Chugaev <elov4anin@gmail.com>
2021-01-10 22:20:16 +0500

    game page -  components

--- src/app/pages/game/components/difficulty/difficulty.component.ts
+++ src/app/pages/game/components/difficulty/difficulty.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {IDifficulty} from "../difficulty.interface";
+import {IDifficulty} from "../../difficulty.interface";
 
 @Component({
   selector: 'app-difficulty',
--- src/app/pages/game/game.module.ts
+++ src/app/pages/game/game.module.ts
@@ -4,8 +4,8 @@ import {GamePageRoutingModule} from './game-routing.module';
 
 import {GamePage} from './game.page';
 import {SharedModule} from "../../shared/shared.module";
-import {DifficultyComponent} from "./difficulty/difficulty.component";
-import {PersonComponent} from "./person/person.component";
+import {DifficultyComponent} from "./components/difficulty/difficulty.component";
+import {PersonComponent} from "./components/person/person.component";
 
 @NgModule({
     imports: [
