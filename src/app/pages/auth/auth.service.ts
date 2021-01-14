f1ab158d8d828eadf8afcdcce297ba3cd49be430
Sergey Chugaev <elov4anin@gmail.com>
2021-01-15 01:20:44 +0500

    register method

--- src/app/pages/auth/auth.service.ts
+++ src/app/pages/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {SITE_MAIN} from "../../shared/configs/main.config";
 
@@ -8,40 +8,51 @@ import {Observable} from "rxjs";
 import {ILoginResponse} from "../../shared/interfaces/auth.interfaces";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private _http: HttpClient) { }
+    constructor(private _http: HttpClient) {
+    }
 
-  login(params: {email: string, password: string}): Observable<ILoginResponse> {
-   // const hash = md5(params.password).toString();
-   // const salt = sha1(hash).toString();
-    return this._http.post<ILoginResponse>(
-        SITE_MAIN + 'integration/sport80-login.php' ,
-        {email: params.email, hash: params.password},
-        {
-          headers: this._getHeader(),
-        }
+    login(params: { email: string, password: string }): Observable<ILoginResponse> {
+        // const hash = md5(params.password).toString();
+        // const salt = sha1(hash).toString();
+        return this._http.post<ILoginResponse>(
+            SITE_MAIN + 'integration/sport80-login.php',
+            {email: params.email, hash: params.password},
+            {
+                headers: this._getHeader(),
+            }
         );
-  }
+    }
 
-  forgot(params: {email: string}) {
-      return this._http.post<ILoginResponse>(
-          SITE_MAIN + 'integration/sport80-reset-pass.php' ,
-          {email: params.email},
-          {
-              headers: this._getHeader(),
-          }
-      );
-  }
+    register(params): Observable<ILoginResponse> {
+        return this._http.post<ILoginResponse>(
+            SITE_MAIN + 'integration/sport80-create-account.php',
+            params,
+            {
+                headers: this._getHeader(),
+            }
+        );
+    }
 
-  private _getHeader() {
-    return new HttpHeaders(
-        {
-          'Accept': '*/*',
-          'Content-Type': 'application/json',
-        }
-    );
-  }
+    forgot(params: { email: string }) {
+        return this._http.post<ILoginResponse>(
+            SITE_MAIN + 'integration/sport80-reset-pass.php',
+            {email: params.email},
+            {
+                headers: this._getHeader(),
+            }
+        );
+    }
+
+    private _getHeader() {
+        return new HttpHeaders(
+            {
+                'Accept': '*/*',
+                'Content-Type': 'application/json',
+            }
+        );
+    }
 }
--- src/app/pages/auth/reg/reg.page.ts
+++ src/app/pages/auth/reg/reg.page.ts
@@ -2,6 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import {AuthRoutesEnum} from "../auth-routes.enum";
+import {AuthService} from "../auth.service";
+import {RESPONSE_CODES} from "../../../shared/configs/response.constants";
+import {StorageEnum} from "../../../shared/enums/Storage.enum";
+import {CoreStore} from "../../../shared/store/core.store";
+import {ToastNotificationService} from "../../../shared/helpers/toast-notification.service";
 
 @Component({
     selector: 'app-reg',
@@ -10,22 +15,36 @@ import {AuthRoutesEnum} from "../auth-routes.enum";
 })
 export class RegPage implements OnInit {
     form: FormGroup = new FormGroup({
-        firstName: new FormControl('', [Validators.required]),
-        lastName: new FormControl('', [Validators.required]),
-        email: new FormControl('', [Validators.required]),
-        password: new FormControl('', [Validators.required]),
-        confirmPassword: new FormControl('', [Validators.required]),
-        date: new FormControl('', [Validators.required]),
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        pwd: new FormControl('', [Validators.required,Validators.minLength(8)]),
+        confirmPassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
+        dob: new FormControl(''),
     });
 
-    constructor(private _router: Router) {
+    constructor(
+        private _router: Router,
+        private _authService: AuthService,
+        private _coreStore: CoreStore,
+        private _toast: ToastNotificationService,
+        ) {
     }
 
     ngOnInit() {
     }
 
     register() {
-
+        this._authService.register(this.form.value).subscribe(async (res) => {
+                if (res.response_code === RESPONSE_CODES.SUCCESS) {
+                    await this._coreStore.setValue(StorageEnum.LOGGEDIN, true)
+                    await this._coreStore.setValue(StorageEnum.PROFILE, res.user)
+                    await this._router.navigate(['/']);
+                } else {
+                    await this._toast.error(res.response_msg);
+                }
+            }
+        )
     }
 
     async openLogin() {
