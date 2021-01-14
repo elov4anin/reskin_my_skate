a0f944a01591044cc96427bb7603d885843a7247
Sergey Chugaev <elov4anin@gmail.com>
2021-01-14 22:41:49 +0500

    reg.module.ts

--- src/app/pages/auth/reg/reg.module.ts
+++ src/app/pages/auth/reg/reg.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { RegPageRoutingModule } from './reg-routing.module';
 
 import { RegPage } from './reg.page';
+import {SharedModule} from "../../../shared/shared.module";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RegPageRoutingModule
-  ],
+    imports: [
+        SharedModule,
+        RegPageRoutingModule,
+        ReactiveFormsModule
+    ],
   declarations: [RegPage]
 })
 export class RegPageModule {}
--- src/app/pages/auth/reg/reg.page.ts
+++ src/app/pages/auth/reg/reg.page.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {AuthRoutesEnum} from "../auth-routes.enum";
 
 @Component({
-  selector: 'app-reg',
-  templateUrl: './reg.page.html',
-  styleUrls: ['./reg.page.scss'],
+    selector: 'app-reg',
+    templateUrl: './reg.page.html',
+    styleUrls: ['./reg.page.scss'],
 })
 export class RegPage implements OnInit {
+    form: FormGroup = new FormGroup({
+        firstName: new FormControl('', [Validators.required]),
+        lastName: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required]),
+        confirmPassword: new FormControl('', [Validators.required]),
+        date: new FormControl('', [Validators.required]),
+    });
 
-  constructor() { }
+    constructor(private _router: Router) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    register() {
+
+    }
+
+    async openLogin() {
+        await this._router.navigate(['/', AuthRoutesEnum.ROOT, AuthRoutesEnum.LOGIN])
+    }
 }
