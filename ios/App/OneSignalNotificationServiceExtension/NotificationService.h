ecc4843600ad52a002f0386c082410b7a06d9ac3
Сергей <serg@192.168.199.128>
2021-02-09 01:00:24 +0500

    add pushes ios

---
+++ ios/App/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Сергей on 09.02.2021.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
