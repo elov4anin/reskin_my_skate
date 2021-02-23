21df5c9fca630057e5fc0861beaeff89b6b41b5f
Sergey Chugaev <elov4anin@gmail.com>
2021-02-23 18:59:37 +0500

    add spot from skatepark page

--- src/app/tabs/skateparks/components/add-spot-board/add-spot-board.component.ts
+++ src/app/tabs/skateparks/components/add-spot-board/add-spot-board.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {ModalAddSpotComponent} from '../../../spots/modal-add-spot/modal-add-spot.component';
+import {ModalController} from '@ionic/angular';
 
 @Component({
   selector: 'app-add-spot-board',
   templateUrl: './add-spot-board.component.html',
   styleUrls: ['./add-spot-board.component.scss'],
 })
-export class AddSpotBoardComponent implements OnInit {
+export class AddSpotBoardComponent{
 
-  constructor() { }
+  constructor(private _modalController: ModalController) { }
 
-  ngOnInit() {}
+  async openAddSpotModal() {
+    const modal = await this._modalController.create({
+      component: ModalAddSpotComponent,
+      cssClass: 'modal-add-spot'
+    });
+    return await modal.present();
+  }
 
 }
