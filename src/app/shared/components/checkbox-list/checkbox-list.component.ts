db77a25ac252216dd6966c55b279391091a33c8c
Sergey Chugaev <elov4anin@gmail.com>
2021-04-07 22:06:31 +0500

    fix •uncheck tricks

--- src/app/shared/components/checkbox-list/checkbox-list.component.ts
+++ src/app/shared/components/checkbox-list/checkbox-list.component.ts
@@ -71,12 +71,10 @@ export class CheckboxListComponent implements ControlValueAccessor {
   change(ch: IFeatureSkatepark) {
     ch.checked = !ch.checked;
     if (ch.checked) {
-      console.log('ch', ch);
       this.value.push(ch.value);
     } else {
       this.value = this.value.filter(v => v !== ch.value);
     }
-    console.log(this.value);
     this.writeValue(this.value);
    // ch.checked = !ch.checked;
     // this.value = this.checkboxes.filter(c => c.checked).map(v => v.value);
